90b9db7a0da5133f78af3ebc9db2e8e35b06a72e
JVPrice <julionprice@hotmail.com>
2023-05-08 09:22:26 +0900

    Create searchbar with different syntax

--- src/components/search/searchbar.js
+++ src/components/search/searchbar.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 export class Searchbar extends React.Component {
     render() {
         return 
     }
 };
+
+
+const searchBar = () => {
+    const [searchInput, setSearchInput] = useState("");
+    const subreddits = [];
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        setSearchInput(e.target.value);
+      };
+    if (searchInput.length > 0) {
+        subreddits.filter((subreddit) => {
+        return subreddit.name.match(searchInput);
+    });
+    };
+
+    return ( 
+        <div>
+            <input type="search" placeholder="Search Subreddit" onChange={handleChange} value={searchInput} />
+            
+        </div>
+    )
+}
+
+export default searchBar;
